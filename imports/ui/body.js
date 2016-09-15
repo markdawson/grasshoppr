d140213a09e188af8136791f6e35f73f28bbf7b7
Mark Dawson <mark@markdawson.xyz>
2016-09-15 16:00:00 -0400

    first commit

---
+++ imports/ui/body.js
@@ -0,0 +1,22 @@
+import { Template } from 'meteor/templating';
+ 
+import './body.html';
+import './form.html';
+
+Template.form.onRendered({
+
+  var slider = document.getElementById('test5');
+  noUiSlider.create(slider, {
+   start: [20, 80],
+   connect: true,
+   step: 1,
+   range: {
+     'min': 0,
+     'max': 100
+   },
+   format: wNumb({
+     decimals: 0
+   })
+  });
+
+});
---
+++ imports/ui/form.js
@@ -0,0 +1,5 @@
+import { Template } from 'meteor/templating';
+
+import './form.html';
+
+
