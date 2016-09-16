93095114e5a0c801d469667c3bf062789856b2dd
Mark Dawson <mark@markdawson.xyz>
2016-09-15 22:08:42 -0400

    created collection

---
+++ client/layouts/MainLayout.js
@@ -0,0 +1,16 @@
+Template.MainLayout.onRendered({
+  // $('.chips').material_chip();
+  // $('.chips-initial').material_chip({
+  //   data: [{
+  //     tag: 'Apple',
+  //   }, {
+  //     tag: 'Microsoft',
+  //   }, {
+  //     tag: 'Google',
+  //   }],
+  // });
+  // $('.chips-placeholder').material_chip({
+  //   placeholder: 'Enter a tag',
+  //   secondaryPlaceholder: '+Tag',
+  // });
+});
---
+++ entries.js
@@ -0,0 +1,42 @@
+Entries = new Mongo.Collection('entries');
+
+Entries.allow({
+	insert: function(userId, doc) {
+		return !!userId;
+	}
+});
+
+EntrySchema = new SimpleSchema({
+	how_was_today: {
+		type: Number
+	},
+	focus: {
+		type: Number
+	},
+	people: {
+		type: String
+	},
+	thought: {
+		type: String
+	},
+	createdAt: {
+		type: Date,
+		autoValue: function() {
+			return new Date()
+		},
+		autoform: {
+			type: "hidden"
+		}
+	},
+	createdBy: {
+		type: String,
+		autoValue: function() {
+			return this.userId
+		}
+		autoform: {
+			type: "hidden"
+		}
+	}
+});
+
+Entries.attachSchema(EntrySchema);
