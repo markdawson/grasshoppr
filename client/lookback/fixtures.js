6dd6cfbca0c386fe134fec57cefb077e325349e3
Mark Dawson <mark@markdawson.xyz>
2016-09-21 17:06:17 -0400

    finally finished messing with fixtures (for now)

--- client/lookback/fixtures.js
+++ client/lookback/fixtures.js
@@ -2,7 +2,7 @@ let date = new Date();
 
 export const fixture_data = [
 	{
-		selectedDate: String(Date(date.setDate(date.getDate() - 1))).substr(0, 15),
+		selectedDate: String(new Date(date.setDate(date.getDate() - 1))).substr(0,15),
 		selectedDateParse: Date.parse(date),
 		how_was_today: 8,
 		focus: 4,
@@ -26,7 +26,7 @@ export const fixture_data = [
 		thought: "Phew I'm finally done with my midterm! Finished that at 2pm today and just spent the rest of the day hanging out with Frank and Savannah. No better way to spend an afternoon :)"
 	},
 	{
-		selectedDate: String(Date(date.setDate(date.getDate() - 1))).substr(0, 15),
+		selectedDate: String(new Date(date.setDate(date.getDate() - 1))).substr(0,15),
 		selectedDateParse: Date.parse(date),
 		how_was_today: 7,
 		focus: 9,
@@ -47,7 +47,7 @@ export const fixture_data = [
 		thought: "I am in full study mode for my economics midterm! I really got kill it!"
 	},
 	{
-		selectedDate: String(Date(date.setDate(date.getDate() - 1))).substr(0, 15),
+		selectedDate: String(new Date(date.setDate(date.getDate() - 1))).substr(0,15),
 		selectedDateParse: Date.parse(date),
 		how_was_today: 6,
 		focus: 5,
@@ -66,5 +66,50 @@ export const fixture_data = [
 	                },             
 	              ],
 		thought: "It was great getting coffee with Professor Maurel today. He is such a great guy!"
+	},
+	{
+		selectedDate: String(new Date(date.setDate(date.getDate() - 1))).substr(0,15),
+		selectedDateParse: Date.parse(date),
+		how_was_today: 8,
+		focus: 7,
+		people: [
+	                {
+	                  tag: 'Joseph',
+	                },
+	                {
+	                  tag: 'Chinmay',
+	                },                
+	                {
+	                  tag: 'Frank',
+	                },
+	                {
+	                  tag: 'Arielle',
+	                },             
+	              ],
+		thought: "I spent most of today working on a paper for my philosophy class. Also I enjoyed lunch with Chinmay and Joseph."
+	},
+	{
+		selectedDate: String(new Date(date.setDate(date.getDate() - 1))).substr(0,15),
+		selectedDateParse: Date.parse(date),
+		how_was_today: 7,
+		focus: 3,
+		people: [
+	                {
+	                  tag: 'Ana',
+	                },
+	                {
+	                  tag: 'Arianna',
+	                },                
+	                {
+	                  tag: 'Claire',
+	                },
+	                {
+	                  tag: 'Ottavia',
+	                }, 
+	                {
+	                  tag: 'Jovonnie',
+	                },               
+	              ],
+		thought: "I went to dinner with Jovonnie and everyone from Ana's house tonight which was great. Afterwards we went to a cat themed party."
 	}
 ];
